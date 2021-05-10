3bdf2e74dd9c87effeb6a3b97241399514811b57
VanyaShestakov <vanka1108@gmail.com>
2021-05-10 11:41:45 +0300

    TeachersParser interface is covered by unit tests(TeachersParserTest.java). Updates tests in ScheduleParserTest.java

--- src/JSON/Parsers/Tests/ScheduleParserTest.java
+++ src/JSON/Parsers/Tests/ScheduleParserTest.java
@@ -9,11 +9,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Optional;
 
 /**
  * Testing by {@code JUnit5}.
@@ -74,16 +77,26 @@ class ScheduleParserTest {
         Assertions.assertEquals(MONDAY_PAIRS_AMOUNT, scheduleList.get(0).size());
     }
 
-    @Test
-    @DisplayName("Any day of the week must contain BSUIRLesson objects")
-    void anyWeekDayMustContainsBSUIRLessonObj() {
-        Assertions.assertSame(BSUIRLesson.class, scheduleList.get(0).get(0).getClass());
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6})
+    @DisplayName("Any day of the week must contain BSUIRLesson objects (expect Saturday)")
+    void anyWeekDayMustContainsBSUIRLessonObj(int weekDay) {
+        if (weekDay != 6)
+            Assertions.assertSame(BSUIRLesson.class, scheduleList.get(weekDay).get(0).getClass());
+        else
+            Assertions.assertThrows(IndexOutOfBoundsException.class, () -> scheduleList.get(weekDay).get(0));
     }
 
     @Test
     @DisplayName("Any lesson must contain BSUIRTeacher object")
     void anyLessonMustContainBSUIRTeacherObj() {
         Assertions.assertSame(BSUIRTeacher.class, scheduleList.get(0).get(0).getTeacher().getClass());
+    }
+
+    @Test
+    @DisplayName("Monday must contain 3 English pairs")
+    void mondayMustContainThreeEnglishPairs() {
+        Assertions.assertEquals(3, scheduleList.get(0).stream().filter(lesson -> lesson.getSubjectName().equals("ИнЯз")).count());
     }
 
     @Test
--- src/JSON/Parsers/Tests/TeachersParserTest.java
+++ src/JSON/Parsers/Tests/TeachersParserTest.java
@@ -8,18 +8,24 @@ import Schedule.BSUIRTeacher;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TeachersParserTest {
 
     private static final String FILE_WITH_JSON_PATH = "src\\JSON\\Parsers\\Tests\\teachersJSON.txt";
+    private static final int TEACHERS_AMOUNT = 25;
+
     private static ArrayList<BSUIRTeacher> teachers;
 
     @BeforeAll
@@ -43,7 +49,28 @@ class TeachersParserTest {
     }
 
     @Test
+    @DisplayName("Amount of teachers must be 25")
     void amountOfTeachersMustBeTwentyFive() {
-        Assertions.assertEquals(25, teachers.size());
+        Assertions.assertEquals(TEACHERS_AMOUNT, teachers.size());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Андрианова", "Андреева", "Анисимов", "Антипенко"})
+    @DisplayName("List of teachers must contains teachers with surnames: Андрианова, Андреева, Анисимов, Антипенко.")
+    void teachersListMustContainsSomeTeachers(String lastName){
+        Optional<BSUIRTeacher> optTeacher = teachers.parallelStream().filter(obj -> obj.getLastName().equals(lastName)).findFirst();
+        Assertions.assertTrue(optTeacher.isPresent());
+    }
+
+    @Test
+    @DisplayName("First teacher must have ID: 500434")
+    void checkFirstTeacher() {
+        Assertions.assertEquals(500434, teachers.get(0).getId());
+    }
+
+    @Test
+    @DisplayName("Last teacher must have ID: 505738")
+    void checkLastTeacher() {
+        Assertions.assertEquals(505738, teachers.get(teachers.size() - 1).getId());
     }
 }
